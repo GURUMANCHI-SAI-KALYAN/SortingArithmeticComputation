dda29bc14e34a931ae97a627942ef62d0de772bf
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2020-12-29 13:57:02 +0530

    adding. uc6 file

--- SortingArithmeticCompuatation.sh
+++ SortingArithmeticCompuatation.sh
@@ -2,5 +2,8 @@
 read -p "enter first number" a
 read -p "enter second number" b 
 read -p "enter third number" c
-Arth=$((a%b+c))
-echo $Arth
+Arth[0]=$((a+b*c))
+Arth[1]=$((a*b+c))
+Arth[2]=$((c+a/b))
+Arth[3]=$((a%b+c))
+echo ${!Arth[@]}
