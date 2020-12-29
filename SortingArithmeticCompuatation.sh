2b141913c91a2164a2ae83367b5e8c8f6eb09f7e
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2020-12-29 17:03:34 +0530

    add.adding uc8

--- SortingArithmeticCompuatation.sh
+++ SortingArithmeticCompuatation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+max=1000
 read -p "enter first number" a
 read -p "enter second number" b 
 read -p "enter third number" c
@@ -10,4 +11,17 @@ array[0]=${Arth[0]}
 array[1]=${Arth[1]}
 array[2]=${Arth[2]}
 array[3]=${Arth[3]}
+echo ${array[@]}
+for (( i=0 ;i<4;i++ ))
+do
+  for (( j=$((i+1));j<4;j++))
+  do
+	if [[ $((array[$i])) -le $((array[$j])) ]]
+	then
+		temp=${array[$i]}
+		array[$i]=$((array[$j]))
+		array[$j]=$temp
+	fi
+done
+done
 echo ${array[@]}
