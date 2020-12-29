42a25f5e0e3586da758dbc022040e97eab2eb7c8
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2020-12-29 14:16:47 +0530

    adding. uc7 file

--- SortingArithmeticCompuatation.sh
+++ SortingArithmeticCompuatation.sh
@@ -6,4 +6,8 @@ Arth[0]=$((a+b*c))
 Arth[1]=$((a*b+c))
 Arth[2]=$((c+a/b))
 Arth[3]=$((a%b+c))
-echo ${!Arth[@]}
+array[0]=${Arth[0]}
+array[1]=${Arth[1]}
+array[2]=${Arth[2]}
+array[3]=${Arth[3]}
+echo ${array[@]}
