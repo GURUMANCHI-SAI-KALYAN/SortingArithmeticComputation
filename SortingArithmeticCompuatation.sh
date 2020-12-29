64761d3aa655b3248393f12cf50564b4dd275ce3
GURUMANCHI-SAI-KALYAN <kalyangurumanchi@gmail.com>
2020-12-29 17:06:26 +0530

    add.adding uc9

--- SortingArithmeticCompuatation.sh
+++ SortingArithmeticCompuatation.sh
@@ -16,7 +16,7 @@ for (( i=0 ;i<4;i++ ))
 do
   for (( j=$((i+1));j<4;j++))
   do
-	if [[ $((array[$i])) -le $((array[$j])) ]]
+	if [[ $((array[$i])) -ge $((array[$j])) ]]
 	then
 		temp=${array[$i]}
 		array[$i]=$((array[$j]))
